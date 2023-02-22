1676cf48694483e08a9e552f44e73704f21a2437
Suraj Nepali <surajnepali7896@gmail.com>
2023-02-22 07:54:29 +0545

    Understood the concept of Cookie in Cypress

--- cypress/integration/cookies_start.js
+++ cypress/integration/cookies_start.js
@@ -1,5 +1,11 @@
 /// <reference types="cypress" />
 
+// Cypress.Cookies.defaults({
+//   preserve: 'trello_token'
+// }) //if we want to stay signed in in every testcase, we can use this command to preserve the cookie in support/index.js
+
+Cypress.Cookies.preserveOnce('trello_token') //if we want to stay signed in only in this file, we can use this command
+
 describe('Cookies', () => {
 
   beforeEach(() => {
@@ -12,25 +18,20 @@ describe('Cookies', () => {
   it.only('test #1', () => {
 
     cy
-      .get('[data-cy="login-menu"]')
-      .click();
-
+      .setCookie('trello_token', '.....') //if we leave it like this, it will be shown in Application/Cookies but not actually signed in
+    
     cy
-      .get('[data-cy="login-email"]')
-      .type('filip@example.com');
+      .reload() //this will reload the page and we will be signed in
 
-    cy
-      .get('[data-cy="login-password"]')
-      .type('Asdf.1234#');
-
-    cy
-      .get('[data-cy="login"]')
-      .click();
 
     });
 
   it('test #2', () => {
-
+    // Cypress.Cookies.preserveOnce('trello_token') //this line of code works up to this testcase 
   });
+
+  it('test #3', () => {
+    // To work in this testcase onward, we need to use Cypress.Cookies.preserveOnce('trello_token') in beforeEach() only for this test file
+  })
 
 });
