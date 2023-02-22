0244501db42c0ae58e73c617eace7ad4556be354
Suraj Nepali <surajnepali7896@gmail.com>
2023-02-22 08:34:12 +0545

    Interception to API request

--- cypress/e2e/intercept_request_start.cy.js
+++ cypress/e2e/intercept_request_start.cy.js
@@ -1,12 +1,49 @@
 /// <reference types="cypress" />
 
+// it('Intercept requests', () => {
+
+//   cy.intercept({
+//     method: 'GET',
+//     url: '/api/boards',
+//   }).as('boardList')
+
+//   cy
+//     .visit('/')
+
+//   cy
+//     .wait('@boardList')
+//     .its('response.statusCode')
+//     .should('eq', 200)
+
+//   cy
+//     .get('[data-cy=board-item]')
+//     .should('have.length', 1)
+
+// });
+
 it('Intercept requests', () => {
+
+  cy.intercept({
+    method: 'POST',
+    url: '/api/boards',
+  }).as('addInBoard')
 
   cy
     .visit('/')
+  
+  cy
+    .get('[data-cy=create-board]')
+    .click()
+  
+  cy
+    .get('[data-cy=new-board-input]')
+    .type('board created by Suraj{enter}')
 
   cy
-    .get('[data-cy=board-item]')
-    .should('have.length', 0)
+    .wait('@addInBoard')
+    .then((interception) => {
+      expect(interception.response.statusCode).to.eq(201)
+      expect(interception.response.body.name).to.eq('board created by Suraj')
+    })
 
 });
